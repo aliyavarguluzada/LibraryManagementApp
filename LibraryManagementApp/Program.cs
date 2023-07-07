5b6ceba0faf27716daed44c4d86766fa9f9da94c
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-07-07 15:01:04 +0400

    Add project files.

---
+++ LibraryManagementApp/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ 
+ Application Idea: Library Management System
+
+Description:
+Create a console-based library management system where users can manage books, library members, and borrowing records.
+
+Features:
+
+Book Management:
+
+Add a new book to the library.
+Update book information (e.g., title, author, genre).
+Remove a book from the library.
+Display a list of all available books.
+Member Management:
+
+Add a new member to the library.
+Update member information (e.g., name, contact details).
+Remove a member from the library.
+Display a list of all library members.
+Borrowing Management:
+
+Allow a member to borrow a book.
+Check if a book is available for borrowing.
+Display a list of all borrowed books and their respective borrowers.
+Allow a member to return a borrowed book.
+Implementation:
+To implement this application, you can create the following classes:
+
+Book:
+
+Properties: book ID, title, author, genre, availability status, etc.
+Methods: GetBookInfo(), UpdateAvailability(), etc.
+Member:
+
+Properties: member ID, name, contact details, etc.
+Methods: GetMemberInfo(), UpdateContactDetails(), etc.
+Library:
+
+Properties: list of books, list of members, etc.
+Methods: AddBook(), RemoveBook(), AddMember(), RemoveMember(), etc.
+BorrowingRecord:
+
+Properties: book, member, issue date, due date, return date, etc.
+Methods: GetRecordInfo(), UpdateReturnDate(), etc.
+The main program can provide a menu-based interface 
+where users can select different options to perform various operations on books, members, and borrowing records. 
+Additionally, you can store the data in memory using appropriate data structures (e.g., lists) 
+or utilize a simple file system for data persistence.
+ 
+ */
+
+
+
+
+
+
+
+
+
